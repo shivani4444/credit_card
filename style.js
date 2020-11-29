21393334bbc91cdc80136b9d135942568f0e0234
Shivani Sharma <shivani.sharma9885@gmail.com>
2020-11-29 16:28:14 +0530

    credit card

--- style.js
+++ style.js
@@ -1,28 +1,27 @@
-var $inp = $(".input-cart-number");
 
+//paste
+$('#tb0').on('paste',function(){
+  var element = this;
+   setTimeout(function () {
+     var splittedString = $('#tb0').val().match(/.{1,4}/g)
+     for(var i=0; i<splittedString.length; i++){
+         $('#tb' + i).val(splittedString[i]).focus();
+     }
+   },0);
+ })
+
+ var $inp = $(".input-cart-number");
 $inp.on({
- input: function(ev) {
+ input: function(ev) { //focus next
     $t = $(this);
   if($t.val().length > 3) {
     $inp.eq($inp.index($t) + 1).focus();
   }
  },
- keydown: function(ev) {
+ keydown: function(ev) { //when delete focus previous
   var i = $inp.index(this);
   if(ev.which===8 && !this.value && i) {
     $inp.eq(i - 1).focus();
   }
- },
- paste: function(e) {
-
-    var pastedData = e.originalEvent.clipboardData.getData('num');
-    var pastedChars = pastedData.split("");
-    
-    var curIndex = $inp.index(this)
-    
-    for (var i=0; i < pastedChars.length; i++) {
-      var char = pastedChars[i]
-      $inp.eq(curIndex + i).val(char).focus();
-    }
  }
 });
